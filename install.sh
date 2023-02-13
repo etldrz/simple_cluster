c37d40b518fb4a9d4528640673bbeba8267d39ef
Axe Tang <tangxiangyao0906@gmail.com>
2023-02-12 23:53:55 -0700

    changed install

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
-sudo apt-get update
-sudo apt update
+sudo apt-get update -y
+sudo apt update -y
 sudo apt install curl git
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
-bash
-. "$HOME/.asdf/asdf.sh"
-. "$HOME/.asdf/completions/asdf.bash"
-tsch
-bash
+bash . "$HOME/.asdf/asdf.sh"
+bash . "$HOME/.asdf/completions/asdf.bash"
 sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-gtk3-dev libwxgtk-webview3.0-gtk3-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
