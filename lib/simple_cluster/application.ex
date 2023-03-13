4ee9dad6bade0d01751cfacc2087e3b8b459ad55
Axe Tang <axe.tang@utah.edu>
2023-03-12 18:37:08 -0600

    Changed grouping protocol

--- lib/simple_cluster/application.ex
+++ lib/simple_cluster/application.ex
@@ -9,13 +9,25 @@ defmodule SimpleCluster.Application do
   def start(_type, _args) do
     Node.set_cookie(:cluster1)
     topologies = [
-      example: [
-        strategy: Cluster.Strategy.Epmd,
-        config: [hosts: [:"n1@155.98.38.91", :"n2@155.98.38.99", :"n3@155.98.38.97", :"n4@73.98.154.254"]]
-        #config: [hosts: [:"n1@192.168.1.1", :"n2@192.168.1.2", :"n3@192.168.1.3"]]
-        #config: [hosts: [:"n1@127.0.0.1", :"n2@127.0.0.1"]]
+      gossip_example: [
+        strategy: Elixir.Cluster.Strategy.Gossip,
+        config: [
+          port: 45892,
+          if_addr: "0.0.0.0",
+          multicast_if: "192.168.1.1",
+          multicast_addr: "233.252.1.32",
+          multicast_ttl: 1
+        ]
       ]
     ]
+    # topologies = [
+    #   example: [
+    #     strategy: Cluster.Strategy.Epmd,
+    #     config: [hosts: [:"n1@155.98.38.91", :"n2@155.98.38.99", :"n3@155.98.38.97", :"n4@73.98.154.254"]]
+    #     #config: [hosts: [:"n1@192.168.1.1", :"n2@192.168.1.2", :"n3@192.168.1.3"]]
+    #     #config: [hosts: [:"n1@127.0.0.1", :"n2@127.0.0.1"]]
+    #   ]
+    # ]
     children = [
       {Cluster.Supervisor, [topologies, [name: MyApp.ClusterSupervisor]]},
       SimpleCluster.Observer,
