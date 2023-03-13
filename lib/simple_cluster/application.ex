f8222f05b91da00cfe1f8948799d081eecf489aa
Axe Tang <axe.tang@utah.edu>
2023-03-12 18:01:11 -0600

    Changed addresses of nodes

--- lib/simple_cluster/application.ex
+++ lib/simple_cluster/application.ex
@@ -7,11 +7,11 @@ defmodule SimpleCluster.Application do
 
   @impl true
   def start(_type, _args) do
-    Node.set_cookie(:cluster1);
+    Node.set_cookie(:cluster1)
     topologies = [
       example: [
         strategy: Cluster.Strategy.Epmd,
-        config: [hosts: [:"n1@155.98.38.110", :"n2@155.98.38.91", :"n3@155.98.38.92", :"n4@73.98.154.254"]]
+        config: [hosts: [:"n1@155.98.38.91", :"n2@155.98.38.99", :"n3@155.98.38.97", :"n4@73.98.154.254"]]
         #config: [hosts: [:"n1@192.168.1.1", :"n2@192.168.1.2", :"n3@192.168.1.3"]]
         #config: [hosts: [:"n1@127.0.0.1", :"n2@127.0.0.1"]]
       ]
@@ -26,5 +26,6 @@ defmodule SimpleCluster.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SimpleCluster.Supervisor]
     Supervisor.start_link(children, opts)
+
   end
 end
