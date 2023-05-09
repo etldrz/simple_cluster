397f30c1446928208bb9c22e3135d567cc2a6a66
Axe Tang <axe.tang@utah.edu>
2023-05-09 14:56:28 -0600

    Add functionality to receive output from async processes. Add functionality to send new data to async processes.

--- lib/simple_cluster/executer.ex
+++ lib/simple_cluster/executer.ex
@@ -6,17 +6,20 @@ defmodule SimpleCluster.Executer do
   end
 
   # Run the given command either synchronous or asynchronous.
+  # Will return the result even if async is true.
+  # The user can send new data to async process via send_to_process() function.
+  # However, the user will not be able send new data to synchronous processes (since it will be blocking)
   # DOES NOT GIVE FINE CONTROL OVER THE COMMAND
   def run_command(cmd, async) do
     if async == true do
       # Since it is async, then the caller wouldn't care about the return value
-      {:ok, _ , osPID} = :exec.run_link(cmd, [])
+      {:ok, _ , osPID} = :exec.run_link(cmd, [{:stdout, &handle_async_result/3}, :stdin])
       # So return the osPID so that the caller could use it for future use.
       osPID
     else
       {:ok, [stdout: result]} = :exec.run_link(cmd, [:sync, :stdout])
       # Just return the result
-      result |> parse_output
+      Enum.each(result, &parse_output/1)
     end
   end
 
@@ -52,6 +55,10 @@ defmodule SimpleCluster.Executer do
 
   # This helper function parse the output so that each element is on their own line.
   defp parse_output(output) do
-    List.first(output) |> String.split("\n", trim: true) |> Enum.each(&IO.puts/1)
+    output |> String.split("\n") |> Enum.each(&IO.puts/1)
+  end
+
+  defp handle_async_result(_, _, data) do
+    IO.puts(data)
   end
 end
