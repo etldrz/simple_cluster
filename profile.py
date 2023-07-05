b31147ae3e704a7f5455f84ac914588a546a2d25
Axe Tang <axe.tang@utah.edu>
2023-07-05 14:37:10 -0600

    Added ansible support

--- profile.py
+++ profile.py
@@ -7,20 +7,34 @@ Click on any node in the topology and choose the `shell` menu item. When your sh
 import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
+# Import the Emulab specific extensions.
+import geni.rspec.emulab as emulab
+# Import Emulab Ansible-specific extensions.
+import geni.rspec.emulab.ansible
+from geni.rspec.emulab.ansible import (Role, RoleBinding, Override, Playbook)
 
+HEAD_CMD = "sudo -u `geni-get user_urn | cut -f4 -d+` -Hi /bin/sh -c '/local/repository/emulab-ansible-bootstrap/head.sh >/local/logs/setup.log 2>&1'"
+CLIENT_CMD = "sudo -u `geni-get user_urn | cut -f4 -d+` -Hi /bin/sh -c '/local/repository/emulab-ansible-bootstrap/client.sh >/local/logs/setup.log 2>&1'"
 NODETYPE = "d710"
 NODEIMAGE = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+
 
 # Create a portal context.
 pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
+# Add some custom roles to the rspec; we will bind them to nodes later.
+request.addRole(
+    Role("setup",path="ansible",playbooks=[
+        Playbook("setup",path="setup.yml",become=None)]))
 
 # Add a raw PC to the request and give it an interface.
 node1 = request.RawPC("node1")
 node1.hardware_type = NODETYPE
 node1.disk_image = NODEIMAGE
+node1.addService(pg.Execute(shell="sh",command=HEAD_CMD))
+node1.bindRole(RoleBinding("setup"))
 iface1 = node1.addInterface()
 
 # Specify the IPv4 address
@@ -30,6 +44,8 @@ iface1.addAddress(pg.IPv4Address("192.168.1.1", "255.255.255.0"))
 node2 = request.RawPC("node2")
 node2.hardware_type = NODETYPE
 node2.disk_image = NODEIMAGE
+node2.addService(pg.Execute(shell="sh",command=CLIENT_CMD))
+node2.bindRole(RoleBinding("setup"))
 iface2 = node2.addInterface()
 
 # Specify the IPv4 address
@@ -39,6 +55,8 @@ iface2.addAddress(pg.IPv4Address("192.168.1.2", "255.255.255.0"))
 node3 = request.RawPC("node3")
 node3.hardware_type = NODETYPE
 node3.disk_image = NODEIMAGE
+node3.addService(pg.Execute(shell="sh",command=CLIENT_CMD))
+node3.bindRole(RoleBinding("setup"))
 iface3 = node3.addInterface()
 
 # Specify the IPv4 address
